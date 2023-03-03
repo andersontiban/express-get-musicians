7766571b778814d6c61fd71a1c2a3a48db9d5573
Anderson Tiban <andytiban12@gmail.com>
2023-03-03 11:59:34 -0500

    completed

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const {sequelize} = require("./db");
+const { response } = require("express");
 
 const port = 3000;
 
 //TODO
+app.get("/musicians", async(request, response)=>{
+    let allMusicians = await Musician.findAll();
+    response.json(allMusicians);
+})
 
 app.listen(port, () => {
     sequelize.sync();
