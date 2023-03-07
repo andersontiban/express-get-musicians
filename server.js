2996b82df1fd061fae6ac6001f88c4433045e9eb
Anderson Tiban <andytiban12@gmail.com>
2023-03-06 19:55:38 -0500

    completed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { response } = require("express");
 
 const port = 3000;
 
-//TODO
+//get method, all musicians
 app.get("/musicians", async(request, response)=>{
     let allMusicians = await Musician.findAll();
     response.json(allMusicians);
@@ -17,6 +17,36 @@ app.get("/musicians/:id", async(request, response)=>{
     let musician = await Musician.findByPk(request.params.id)
     response.json(musician);
 })
+
+//middleware
+app.use(express.json());
+
+//post method/ add new musician
+app.post("/musicians", async(request, response)=>{
+    let musicianInfo = request.body;
+    await Musician.create({
+        name: musicianInfo.name,
+        instrument: musicianInfo.instrument
+    })
+})
+//PUT method/update musician
+app.put("/musicians/:id", async(request, response)=>{
+    let primaryKey = request.params.id;
+    let newInfo = request.body;
+    await Musician.update({
+        name: newInfo.name,
+        instrument: newInfo.instrument},{
+            where : {id : primaryKey}
+    })
+})
+//DELETE method
+app.delete("/musicians/:id", async(request, response)=>{
+    let primaryKey = request.params.id;
+    await Musician.destroy({
+        where: {id: primaryKey}
+    });
+})
+
 
 app.listen(port, () => {
     sequelize.sync();
